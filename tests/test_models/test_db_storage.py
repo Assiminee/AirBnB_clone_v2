59dbef84a06f5181468a09dd6b93c1a68813239d
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-02-21 14:50:02 -0800

    unittest: file storage

--- tests/test_models/test_db_storage.py
+++ tests/test_models/test_db_storage.py
@@ -20,3 +20,22 @@ DBStorage = db_storage.DBStorage
 classes = {"Amenity": Amenity, "City": City, "Place": Place,
            "Review": Review, "State": State, "User": User}
 storage_t = os.getenv("HBNB_TYPE_STORAGE")
+
+class TestFileStorage(unittest.TestCase):
+    """Tests for the FileStorage class"""
+    @unittest.skipIf(storage_t != 'db', "not testing db storage")
+    def test_all_returns_dict(self):
+        """ Test if  all returns a dictionaty"""
+        self.assertIs(type(models.storage.all()), dict)
+
+    @unittest.skipIf(storage_t != 'db', "not testing db storage")
+    def test_all_no_class(self):
+        """ Test that all returns all rows when no class is passed"""
+
+    @unittest.skipIf(storage_t != 'db', "not testing db storage")
+    def test_new(self):
+        """ Test that new adds an object to the database"""
+
+    @unittest.skipIf(storage_t != 'db', "not testing db storage")
+    def test_save(self):
+        """ Test that save properly saves objects to file.json"""
