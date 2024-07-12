874fa441732908dd416af456a8bc6a757b605d4f
Assiminee <znatni.yasmine@gmail.com>
2024-07-12 17:33:06 +0100

    msg

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,11 +20,11 @@ html=\
 [ -d /data/web_static/releases/ ] || mkdir /data/web_static/releases/
 [ -d /data/web_static/shared/ ] || mkdir /data/web_static/shared/
 [ -d /data/web_static/releases/test/ ] || mkdir /data/web_static/releases/test/
-[ -f /data/web_static/releases/test/index.html ] || echo -e $html | sudo tee /data/web_static/releases/test/index.html
+[ -f /data/web_static/releases/test/index.html ] || echo -e "$html" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data
 
-sudo sed /rewrite/a\\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t} /etc/nginx/sites-enabled
+sudo sed '/rewrite/a\\tlocation \/hbnb_static {\n\t\talias \/data\/web_static\/current\/;\n\t}' /etc/nginx/sites-enabled
 
 sudo nginx -s reload
