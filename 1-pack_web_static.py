923f8980614dd3ea82756de8b8f673d8940f7b6b
Assiminee <znatni.yasmine@gmail.com>
2024-07-12 19:37:49 +0100

    msg

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from fabric.api import cd, local
+from fabric.api import local
 
 
 def do_pack():
@@ -9,17 +9,16 @@ def do_pack():
         into an archive where the name is in the format
         web_static_<year><month><day><hour><minute><second>.tgz
     """
-    with cd("/data/web_static"):
-        local("[ -d versions ] || mkdir versions")
-        datetime = local(
-            """
-            timedatectl | awk 'NR==1 {
-            gsub(\"-\", \"\");
-            gsub(\":\", \"\");
-            print $4 $5}'
-            """
-        )
-        res = local(f"tar -cvzf versions/web_static_${datetime}.tgz ./*")
-        if res:
-            return res
-        return None
+    local("[ -d versions ] || mkdir versions")
+    datetime = local(
+        """
+        timedatectl | awk 'NR==1 {
+        gsub(\"-\", \"\");
+        gsub(\":\", \"\");
+        print $4 $5}'
+        """
+    )
+    res = local(f"tar -cvzf versions/web_static_${datetime}.tgz ./*")
+    if res:
+        return res
+    return None
