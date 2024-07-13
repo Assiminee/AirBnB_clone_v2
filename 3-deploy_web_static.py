7cd2b69964aa49446fb223728d02e3f49569ff33
Assiminee <znatni.yasmine@gmail.com>
2024-07-13 16:56:25 +0100

    msg

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 
-from fabric.api import cd, local, put, run, sudo
+from fabric.api import local, put, run, env
 from datetime import datetime
-from os.path import isfile
-from fabric.io import env
+from os.path import exists
 
 
 def do_pack():
@@ -23,25 +22,41 @@ def do_pack():
         return res
     return None
 
+
 def do_deploy(archive_path):
     """
         distributes an archive to your web servers
     """
-    if not isfile(archive_path):
+    if not exists(archive_path):
         return False
 
     env.hosts = ['52.3.243.178', '54.197.105.101']
-
     archive_file = archive_path.split('/')[1]
     filename = archive_file.split('.')[0]
     destination_folder = f"/data/web_static/releases/{filename}"
 
     try:
         put(archive_path, '/tmp')
-        sudo(f"mkdir -p {destination_folder}")
-        sudo(f"tar -xvzf /tmp/{archive_file} -C {destination_folder}")
-        sudo(f"rm -rf /tmp/{archive_file} /data/web_static/current")
-        sudo(f"ln -sf {destination_folder} /data/web_static/current")
+        run(f"mkdir -p {destination_folder}")
+        run(f"tar -xzf /tmp/{archive_file} -C {destination_folder}")
+        run(f"mv {destination_folder}/web_static/* {destination_folder}")
+        run(f"rm -rf /tmp/{archive_file}")
+        run(f"rm -rf /data/web_static/current")
+        run(f"rm -rf {destination_folder}/web_static")
+        run(f"ln -sf {destination_folder}/ /data/web_static/current")
         return True
     except Exception:
         return False
+
+
+def deploy():
+    """
+        executes do_pack() and do_deploy()
+        to deploy web_static to the servers
+    """
+    archive_path = do_pack()
+
+    if not archive_path:
+        return False
+
+    return do_deploy(archive_path)
