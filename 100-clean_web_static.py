ebfa120446ce33498f0cffa2a0f31aeb240694bb
Assiminee <znatni.yasmine@gmail.com>
2024-07-13 20:49:48 +0100

    msg

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,25 +12,11 @@ def do_clean(number=0):
          number: the number of the archives,
          including the most recent, to keep
     """
-    try:
-        number = int(number)
-    except Exception:
-        return False
-
+    number = int(number)
     start = number if number > 1 else 1
 
     with lcd("versions"):
-        local_list = local("ls -t", capture=True)
-        local_files = local_list.split()
-
-        for i in range(start, len(local_files)):
-            local(f"rm {local_files[i]}")
+        local(f"ls -t | tail -n +{number} | xargs rm -rf")
 
     with cd("/data/web_static/releases/"):
-        remote_list = run("ls -t")
-        remote_files = remote_list.split()
-
-        for i in range(start, len(remote_files)):
-            if not remote_files[i].startswith("web_static_"):
-                continue
-            sudo(f"rm {remote_files[i]}")
+        run(f"ls -t | tail -n +{number} | xargs sudo rm -rf")
