5d677b007c8c597157ba11913a88aefd1b4fb08d
Assiminee <znatni.yasmine@gmail.com>
2024-07-27 22:35:27 +0100

    msg

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -75,3 +75,7 @@ class DBStorage:
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """closes the class Session"""
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,3 +61,7 @@ class FileStorage:
             key = obj.__class__.__name__ + "." + obj.id
             if key in FileStorage.__objects:
                 FileStorage.__objects.pop(key)
+
+    def close(self):
+        """method for deserializing the JSON file to objects"""
+        self.reload()
