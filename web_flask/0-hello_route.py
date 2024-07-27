2c48593f940e18fc1ee151c8117fffedf2e4118a
Assiminee <znatni.yasmine@gmail.com>
2024-07-27 11:07:37 +0100

    msg

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,14 @@ starts a Flask web application
 
 
 from flask import Flask
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
-def hello_route():
+def hello_hbnb():
+    """Function that returns Hello HBNB!"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
