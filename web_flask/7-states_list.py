c9e6d34f4d7dbb8c40c47e8034caa65ce90959dd
Assiminee <znatni.yasmine@gmail.com>
2024-07-28 12:04:24 +0000

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,22 +6,21 @@ starts a Flask web application
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/states_list')
 def state_list():
     """
     route to a template displaying a list of states
     """
-    states = storage.all(State)
+    states = storage.all('State')
     states = sorted(states.values(), key=lambda state: state.name)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown_sql_session(arg=None):
+def teardown_sql_session(exception):
     """
     Closes SQLAlchemy Session
     """
@@ -29,4 +28,5 @@ def teardown_sql_session(arg=None):
 
 
 if __name__ == "__main__":
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
